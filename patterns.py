8a0408b4efe88145c180d3af8e864bb5b9cfe845
Tony <nya.tony2010@gmail.com>
2024-03-05 09:02:47 +0100

    build: add VSCode setup for CP

--- patterns.py
+++ patterns.py
@@ -1,5 +1,21 @@
 """
 Must do Pattern Problems before starting DSA.
+22 patterns, named like this pattern00...pattern21
+
+Patterns == Nested Loops
+
+For most of the patterns, we have to handle two nested loops:
+an outer loop for rows (lines) & an inner loop for columns.
+
+Below are the 4 steps to follow in order to print any pattern.
+
+Step 1: For the outer loop, count the number of lines (rows)
+Step 2: For the inner loop, focus on the columns and connect them somehow with the rows.
+Step 3: Whatever you have to print, like '*', print them inside the inner loop
+Step 4: Observe symmetry
+
+The first 3 steps are mandatory.
+The last is optional and can be applicable for some patterns.
 """
 import sys
 
@@ -9,7 +25,7 @@ sys.stderr = open("error.txt", "w")
 # your remaining code
 
 
-def pattern1(n: int) -> None:
+def pattern00(n: int) -> None:
 	"""
 	*****
 	*****
@@ -25,7 +41,7 @@ def pattern1(n: int) -> None:
 		print()
 
 
-def pattern2(n: int) -> None:
+def pattern01(n: int) -> None:
 	"""
 	*
 	**
@@ -43,4 +59,4 @@ def pattern2(n: int) -> None:
 
 if __name__ == '__main__':
 	n = int(input())
-	pattern2(n)
+	pattern00(n)
